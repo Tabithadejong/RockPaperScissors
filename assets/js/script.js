393a37e0620b2a92975676f7dcc4f23e56271016
Tabithadejong <tabithadejong654@gmail.com>
2022-10-07 11:57:04 +0000

    Write function to generate computer choice

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const computerChoiceDisplay = document.getElementById("computer-choice")
+const yourChoice = document.getElementById("your-choice")
+const resultDisplay = document.getElementById("result")
+const possibleChoices = document.querySelectorAll("button")
+
+let userChoice 
+let computerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => { 
+ userChoice= e.target.id 
+ yourChoice.innerHTML = userChoice
+ generateComputerChoice()
+})
+    
+)
+
+function generateComputerChoice() { 
+    const randomNumber = Math.floor(Math.random() * 3) +1  
+     
+    if (randomNumber === 1) { 
+        computerChoice = "rock"
+    }
+    if (randomNumber === 2) { 
+        computerChoice = "scissors"
+    }
+    if (randomNumber === 3) { 
+        computerChoice = "paper"
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice 
+}
